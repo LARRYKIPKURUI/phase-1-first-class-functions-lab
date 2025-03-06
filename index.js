8bd6ae6ecb35f7206580a11457df6d93b1c85ff4
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-06 17:37:21 +0300

    functions in depth

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+ // Code your solution in this file!
+ let  drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+ const returnFirstTwoDrivers = function (drivers) {
+    // console.log(`${drivers[0]} ${drivers[1]}  `)
+
+    return [ drivers[0] ,drivers[1]];
+ }
+
+
+ const returnLastTwoDrivers = function (drivers) {
+        return [drivers[2],drivers[3]];
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ console.log(selectingDrivers[0](drivers)); 
+ console.log(selectingDrivers[1](drivers)); 
+
+ function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    };
+}
+
+function fareDoubler (fare) {
+    return fare + fare 
+}
+
+function fareTripler (fare) {
+ return fare * 3
+}
+
+function selectDifferentDrivers(selectingDrivers,functionn) {
+    return functionn(selectingDrivers);
+}
+selectDifferentDrivers(arrayOfDrivers, returnFirstTwoDrivers)
+selectDifferentDrivers(arrayOfDrivers, returnLastTwoDrivers)
